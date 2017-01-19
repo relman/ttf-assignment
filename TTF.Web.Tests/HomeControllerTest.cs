f9444ebe6f3eeabe0e69f52639b4f98b2194c82f
Flint Storman <flint.storman@gmail.com>
2017-01-19 22:35:18 +0600

    TTF.Web.Tests project added

---
+++ TTF.Web.Tests/HomeControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TTF.Services;
+using TTF.Web.Controllers;
+
+namespace TTF.Web.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Test_Index()
+        {
+            var service = new Mock<IMappingService>(MockBehavior.Strict);
+            var controller = new HomeController(service.Object);
+            var result = controller.Index();
+            Assert.IsNotNull(result);
+        }
+    }
+}
